aa4d7c616b1db1ffd37088d16f0863265fb08744
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-03-28 14:09:49 -0300

    sort string

---
+++ sort-string.js
@@ -0,0 +1,29 @@
+const persons = [
+  { name: 'Mariana' },
+  { name: 'Wallace' },
+  { name: 'Maria' },
+  { name: 'Diego' },
+  { name: 'Ana' },
+  { name: 'Larissa' },
+  { name: 'André' },
+  { name: 'José' },
+  { name: 'Jose' },
+];
+
+const sortPersons = persons.sort((person1, person2) => {
+  person1.name.localeCompare(person2.name, 'pt-BR');
+});
+
+console.log(sortPersons);
+
+//[
+// { name: 'Ana' },
+// { name: 'André' },
+// { name: 'Diego' },
+// { name: 'Jose' },
+// { name: 'José' },
+// { name: 'Larissa' },
+// { name: 'Maria' },
+// { name: 'Mariana' },
+// { name: 'Wallace' }
+//]
